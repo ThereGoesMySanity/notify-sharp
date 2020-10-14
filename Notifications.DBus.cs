596401e5a295625a6f1190744e4400e6f50992a6
Will Kennedy <willmlb@sbcglobal.net>
2020-10-14 06:00:37 -0400

    updated to net core

---
+++ Notifications.DBus.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Tmds.DBus;
+
+[assembly: InternalsVisibleTo(Tmds.DBus.Connection.DynamicAssemblyName)]
+namespace Notifications.DBus
+{
+    [DBusInterface("org.xfce.Notifyd")]
+    interface INotifyd : IDBusObject
+    {
+        Task QuitAsync();
+    }
+
+    [DBusInterface("org.freedesktop.Notifications")]
+    interface INotifications : IDBusObject
+    {
+        Task<string[]> GetCapabilitiesAsync();
+        Task<uint> NotifyAsync(string AppName, uint ReplacesId, string AppIcon, string Summary, string Body, string[] Actions, IDictionary<string, object> Hints, int ExpireTimeout);
+        Task CloseNotificationAsync(uint Id);
+        Task<(string name, string vendor, string version, string specVersion)> GetServerInformationAsync();
+        Task<IDisposable> WatchNotificationClosedAsync(Action<(uint id, uint reason)> handler, Action<Exception> onError = null);
+        Task<IDisposable> WatchActionInvokedAsync(Action<(uint id, string actionKey)> handler, Action<Exception> onError = null);
+    }
+}
